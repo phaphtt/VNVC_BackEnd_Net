8b5862e529f0d40c146d2fac0c333b8069bc46a0
Phap Nguyen <phaphtt@gmail.com>
2022-03-31 01:12:32 +0700

    Update API Loyal Customer

---
+++ BackEnd/BackEnd/Controllers/LoyalCustomer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoyalCustomer : Controller
+    {
+        // POST: api/loyalcustomer/insertcustomer
+        [HttpPost("insertcustomer")]
+        public string InsertLoyalCustomer(Models.LoyalCustomer customer)
+        {
+            //Connect DB:
+            Models.MongoDBSettings.ConnectToMongoService();
+            var loyal_customer = Models.MongoDBSettings.database.GetCollection<Models.LoyalCustomer>("LoyalCustomer");
+            int length = (int)loyal_customer.Find(s => s.active == true && s.active == false).Count() + 1;
+            customer.active = true;
+            customer.create_date = DateTime.Today;
+            customer.loyal_customer_id = "LOYALCUSTOMER" + Convert.ToString(length);
+            loyal_customer.InsertOne(customer);
+            return customer.loyal_customer_id;
+        }
+
+        // GET: api/loyalcustomer/getinfo/{loyal_customer_id}
+        [HttpGet("getinfo/{loyal_customer_id}")]
+        public Models.LoyalCustomer GetInfoLoyalCustomer(string loyal_customer_id)
+        {
+            //Connect DB:
+            Models.MongoDBSettings.ConnectToMongoService();
+            var loyal_customer = Models.MongoDBSettings.database.GetCollection<Models.LoyalCustomer>("LoyalCustomer");
+            return loyal_customer.Find(s => s.loyal_customer_id == loyal_customer_id).FirstOrDefault();
+        }
+    }
+}
---
+++ BackEnd/BackEnd/Models/LoyalCustomer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace BackEnd.Models
+{
+    public class LoyalCustomer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+
+        [BsonElement("loyal_customer_id")]
+        public string loyal_customer_id { get; set; }
+
+        [BsonElement("name")]
+        public string name { get; set; }
+
+        [BsonElement("gender")]
+        public string gender { get; set; }
+
+        [BsonElement("phone")]
+        public string phone { get; set; }
+
+        [BsonElement("date_of_birth")]
+        public DateTime date_of_birth { get; set; }
+
+        [BsonElement("cccd")]
+        public string cccd { get; set; }
+
+        [BsonElement("customer_id")]
+        public string customer_id { get; set; }
+
+        [BsonElement("email")]
+        public string email { get; set; }
+
+        [BsonElement("address")]
+        public string address { get; set; }
+
+        [BsonElement("city")]
+        public string city { get; set; }
+
+        [BsonElement("district")]
+        public string district { get; set; }
+
+        [BsonElement("commune")]
+        public string commune { get; set; }
+
+        [BsonElement("active")]
+        public bool active { get; set; }
+
+        [BsonElement("create_date")]
+        public DateTime create_date { get; set; }
+    }
+}
