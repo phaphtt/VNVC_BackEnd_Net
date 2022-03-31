c23e095ae5ec8c37fffb253e9db99efed5d4805c
Phap Nguyen <phaphtt@gmail.com>
2022-03-31 13:54:46 +0700

    Create model Customer, FormRegistor

---
+++ BackEnd/BackEnd/Models/FormRegistor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BackEnd.Models
+{
+    public class CustomerRegistor
+    {
+        public Customer customer { get; set; }
+        public List<Vaccine> vaccines { get; set; }
+    }
+    public class FormRegistor
+    {
+        public List<CustomerRegistor> customers { get; set; }
+        public Int32 total { get; set; }
+
+        [BsonElement("payment_name")]
+        public string payment_name { get; set; }
+
+        [BsonElement("payment_phone")]
+        public string payment_phone { get; set; }
+
+        [BsonElement("payment_email")]
+        public string payment_email { get; set; }
+
+        [BsonElement("payment_cccd")]
+        public string payment_cccd { get; set; }
+
+        [BsonElement("relationship")]
+        public string relationship { get; set; }
+
+        [BsonElement("customer_id")]
+        public string customer_id { get; set; }
+
+        [BsonElement("payment_address")]
+        public string payment_address { get; set; }
+
+        [BsonElement("payment_city")]
+        public string payment_city { get; set; }
+
+        [BsonElement("payment_district")]
+        public string payment_district { get; set; }
+
+        [BsonElement("payment_commune")]
+        public string payment_commune { get; set; }
+
+        [BsonElement("payment_type")]
+        public string payment_type { get; set; }
+
+        [BsonElement("active")]
+        public bool active { get; set; }
+
+        [BsonElement("create_date")]
+        public DateTime create_date { get; set; }
+    }
+}
